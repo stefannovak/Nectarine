89520fa7459b036629c1e0aa997e2b943e97b9b2
Stefan Novak <stefannovak96@gmail.com>
2021-09-01 22:28:05 +0100

    Update PaymentServiceTests.cs

--- nectarineTests/Services/PaymentServiceTests.cs
+++ nectarineTests/Services/PaymentServiceTests.cs
@@ -22,7 +22,7 @@ namespace nectarineTests.Services
             // Configuration setup
             _configurationSection.Setup(x => x.Path).Returns("Stripe");
             _configurationSection.Setup(x => x.Key).Returns("Secret");
-            _configurationSection.Setup(x => x.Value).Returns("sk_test_26PHem9AhJZvU623DfE1x4sd");
+            _configurationSection.Setup(x => x.Value).Returns("");
             StripeConfiguration.ApiKey = _configurationSection.Object.Value;
             
             // PaymentService setup
