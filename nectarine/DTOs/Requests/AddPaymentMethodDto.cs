6d3d95645c1df7fe11003001dab70c1d03477927
Stefan Novak <stefan@peera.co.uk>
2021-09-06 09:36:01 +0100

    fix: AddPaymentMethodDto is now using correct data annotations

--- nectarine/DTOs/Requests/AddPaymentMethodDto.cs
+++ nectarine/DTOs/Requests/AddPaymentMethodDto.cs
@@ -1,20 +1,19 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace nectarineAPI.DTOs.Requests
 {
     public class AddPaymentMethodDto
     {
-        [Range(16,16)]
+        [StringLength(16, MinimumLength = 16)]
         public string CardNumber { get; set; } = string.Empty;
         
-        [Range(2,2)]
+        [Range(1, 12)]
         public int ExpiryMonth { get; set; }
         
-        [Range(2,2)]
+        [Range(2021, 2030)]
         public int ExpiryYear { get; set; }
         
-        [Range(3,4)]
+        [StringLength(4, MinimumLength = 3)]
         public string CVC { get; set; } = string.Empty;
     }
 }
