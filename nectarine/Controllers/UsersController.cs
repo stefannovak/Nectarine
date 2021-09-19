70e00380ac999562fdad43e6c346dc978be2364f
Stefan Novak <stefan@peera.co.uk>
2021-09-19 23:41:50 +0100

    fix: added missing identity migration feat: created CreateUser

--- nectarine/Controllers/UsersController.cs
+++ nectarine/Controllers/UsersController.cs
@@ -1,8 +1,14 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
+using Castle.Core.Internal;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using nectarineAPI.DTOs.Generic;
+using nectarineAPI.DTOs.Requests;
+using nectarineAPI.Models;
+using nectarineAPI.Services;
 using nectarineData.Models;
 
 namespace nectarineAPI.Controllers
@@ -13,11 +19,13 @@ namespace nectarineAPI.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IMapper _mapper;
+        private readonly IUserCustomerService _userCustomerService;
 
-        public UsersController(UserManager<ApplicationUser> userManager, IMapper mapper)
+        public UsersController(UserManager<ApplicationUser> userManager, IMapper mapper, IUserCustomerService userCustomerService)
         {
             _userManager = userManager;
             _mapper = mapper;
+            _userCustomerService = userCustomerService;
         }
         
         /// <summary>
@@ -26,6 +34,50 @@ namespace nectarineAPI.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("GetCurrent")]
-        public async Task<IActionResult> GetCurrent() => Ok(_mapper.Map<UserDTO>(await _userManager.GetUserAsync(User)));
+        public async Task<IActionResult> GetCurrentAsync() => Ok(_mapper.Map<UserDTO>(await _userManager.GetUserAsync(User)));
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateUserAsync(CreateUserDTO createUserDto)
+        {
+            if (createUserDto.Email.IsNullOrEmpty() || createUserDto.Password.IsNullOrEmpty())
+            {
+                return BadRequest(new ApiError {Message = "Email or Password can not be null."});
+            }
+
+            var identityUser = new ApplicationUser
+            {
+                Email = createUserDto.Email,
+                UserName = createUserDto.Email,
+            };
+
+            await _userCustomerService.AddStripeCustomerIdAsync(identityUser);
+
+
+            var result = 
+                await _userManager.CreateAsync(identityUser, createUserDto.Password);
+
+            if (!result.Succeeded)
+            {
+                var dictionary = new Dictionary<string, string>();
+                foreach (IdentityError error in result.Errors)
+                {
+                    dictionary.Add(error.Code, error.Description);
+                }
+
+                return new BadRequestObjectResult(new ApiError { Message = "User creation failed.", Errors = dictionary });
+            }
+            
+            var user = await _userManager.FindByEmailAsync(identityUser.Email);
+            if (user == null)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new ApiError() { Message = "Failed to retrieve new user" }
+                    );
+            }
+
+            return Created($"/Users/{user.Id}", _mapper.Map<UserDTO>(user));
+        }
     }
 }
--- nectarineTests/Controllers/UsersControllerTest.cs
+++ nectarineTests/Controllers/UsersControllerTest.cs
@@ -1,11 +1,16 @@
+using System.Collections.Generic;
 using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using nectarineAPI.Controllers;
 using nectarineAPI.DTOs.Generic;
+using nectarineAPI.DTOs.Requests;
 using nectarineData.Models;
 using Xunit;
 
@@ -19,12 +24,42 @@ namespace nectarineTests.Controllers
         public UsersControllerTest()
         {
             // UserManager setup
-            var store = new Mock<IUserStore<ApplicationUser>>();
-            Mock<UserManager<ApplicationUser>> _userManager = new (store.Object, null, null, null, null, null, null, null, null);
+            var store = new Mock<IUserPasswordStore<ApplicationUser>>();
+            store.Setup(store =>
+                    store.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(IdentityResult.Success));
+            
+            var options = new Mock<IOptions<IdentityOptions>>();
+            var idOptions = new IdentityOptions();
+            idOptions.User.RequireUniqueEmail = true;
+            options.Setup(options => options.Value).Returns(idOptions);
+
+            var userValidators = new List<IUserValidator<ApplicationUser>>();
+            UserValidator<ApplicationUser> validator = new UserValidator<ApplicationUser>();
+            userValidators.Add(validator);
+
+            var passValidator = new PasswordValidator<ApplicationUser>();
+            var pwdValidators = new List<IPasswordValidator<ApplicationUser>>();
+            pwdValidators.Add(passValidator);
+            
+            // UserManager setup
+            Mock<UserManager<ApplicationUser>> _userManager = new (
+                store.Object,
+                options.Object,
+                new PasswordHasher<ApplicationUser>(),
+                userValidators,
+                pwdValidators,
+                new UpperInvariantLookupNormalizer(),
+                new IdentityErrorDescriber(), null,
+                new Mock<ILogger<UserManager<ApplicationUser>>>().Object);
             
             _userManager.Setup(manager => manager
                     .GetUserAsync(It.IsAny<ClaimsPrincipal>()))
                 .ReturnsAsync(new ApplicationUser());
+
+            _userManager.Setup(manager => manager
+                    .CreateAsync(It.IsAny<ApplicationUser>()))
+                .ReturnsAsync(IdentityResult.Success);
             
             // AutoMapper setup
             _mockMapper.Setup(x => x.Map<UserDTO>(It.IsAny<ApplicationUser>()))
@@ -37,14 +72,31 @@ namespace nectarineTests.Controllers
             _controller = new UsersController(_userManager.Object, _mockMapper.Object);
         }
         
-        [Fact(DisplayName = "GetCurrent should get the current user and return an Ok")]
-        public async Task Test_GetCurrentTest()
+        [Fact(DisplayName = "GetCurrentAsync should get the current user and return an Ok")]
+        public async Task Test_GetCurrentAsyncTest()
         {
             // Act
-            var result = await _controller.GetCurrent();
+            var result = await _controller.GetCurrentAsync();
             
             // Arrange
             Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact(DisplayName = "CreateUserAsync should create a user")]
+        public async Task Test_CreateUserAsync()
+        {
+            // Arrange
+            var createUserDto = new CreateUserDTO
+            {
+                Email = "test@test.com",
+                Password = "Password123",
+            };
+            
+            // Act
+            var result = await _controller.CreateUserAsync(createUserDto);
+            
+            // Arrange
+            Assert.IsType<CreatedResult>(result);
         }
 
     }
