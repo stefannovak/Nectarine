5cf4c666cdee5f69cdb5df71987671ce6e1fd9fa
Stefan Novak <stefan@peera.co.uk>
2022-01-01 21:09:31 +0000

    feat: wrote half tests for GoogleService

--- nectarineTests/Services/GoogleServiceTests.cs
+++ nectarineTests/Services/GoogleServiceTests.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Moq;
 using nectarineAPI.Models;
 using nectarineAPI.Services;
 using Xunit;
@@ -8,15 +9,32 @@ namespace nectarineTests.Services
     public class GoogleServiceTests
     {
         private readonly GoogleService<GoogleUser> _subject = new();
+        private readonly Mock<ISocialService<GoogleUser>> _mockSubject = new();
 
+        // TODO: - Figure out this test
         [Fact(DisplayName = "GetUserFromTokenAsync should return a GoogleUser")]
         public async Task Test_GetUserFromTokenAsync_ReturnsGoogleUser()
+        {
+            // Assert
+            _mockSubject
+                .Setup(x => x.GetUserFromTokenAsync(It.IsAny<string>()))
+                .ReturnsAsync(new GoogleUser());
+            
+            // Act
+            var result = await _mockSubject.Object.GetUserFromTokenAsync("token");
+
+            // Assert
+            Assert.IsType<GoogleUser>(result);
+        }
+        
+        [Fact(DisplayName = "GetUserFromTokenAsync should return null")]
+        public async Task Test_GetUserFromTokenAsync_ReturnsNullWhen_InvalidGoogleUser()
         {
             // Act
             var result = await _subject.GetUserFromTokenAsync("token");
 
             // Assert
-            Assert.IsType<GoogleUser>(result);
+            Assert.Null(result);
         }
     }
 }
