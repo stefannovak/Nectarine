3a7b77db495d69856dda665f4ce4285ca2470951
Stefan Novak <stefan@peera.co.uk>
2022-01-15 20:07:44 +0000

    chore: capitalise projects

--- nectarineTests/MockHelpers.cs
+++ nectarineTests/MockHelpers.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 
-namespace nectarineTests
+namespace NectarineTests
 {
     public static class MockHelpers
     {
