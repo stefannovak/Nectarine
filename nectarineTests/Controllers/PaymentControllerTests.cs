21e325b29d71b6c350fac2e462d2c50492978473
Stefan Novak <stefan@peera.co.uk>
2022-01-15 22:38:52 +0000

    feat: PaymentControllerTests improvements

--- nectarineTests/Controllers/PaymentControllerTests.cs
+++ nectarineTests/Controllers/PaymentControllerTests.cs
@@ -1,110 +1,113 @@
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.Extensions.Configuration;
-// using Moq;
-// using NectarineAPI.Controllers;
-// using NectarineAPI.DTOs.Requests;
-// using NectarineAPI.Services;
-// using NectarineData.DataAccess;
-// using NectarineData.Models;
-// using Stripe;
-// using Xunit;
-//
-// namespace NectarineTests.Controllers
-// {
-//     public class PaymentControllerTests
-//     {
-//         private readonly Mock<IConfigurationSection> _configurationSection = new ();
-//         private readonly PaymentController _controller;
-//         private readonly PaymentService _paymentService = new ();
-//         private readonly UserCustomerService _userCustomerService;
-//         private readonly ApplicationUser user = new ();
-//
-//         public PaymentControllerTests()
-//         {
-//             // Configuration setup
-//             _configurationSection.Setup(x => x.Path).Returns("Stripe");
-//             _configurationSection.Setup(x => x.Key).Returns("Secret");
-//             _configurationSection.Setup(x => x.Value).Returns("sk_test_26PHem9AhJZvU623DfE1x4sd");
-//             StripeConfiguration.ApiKey = _configurationSection.Object.Value;
-//
-//             // UserCustomerService setup
-//             var options = new DbContextOptionsBuilder<NectarineDbContext>()
-//                 .UseInMemoryDatabase(databaseName: "TestDb")
-//                 .Options;
-//
-//             NectarineDbContext mockContext = new (options);
-//             _userCustomerService = new UserCustomerService(mockContext);
-//             mockContext.Users.Add(user);
-//
-//             // PaymentController setup
-//             _controller = new PaymentController(mockContext, _paymentService);
-//         }
-//
-//         [Fact(DisplayName = "AddPaymentMethod should add a payment method to the user")]
-//         public async Task Test_AddPaymentMethod()
-//         {
-//             // Arrange
-//             await _userCustomerService.AddStripeCustomerIdAsync(user);
-//
-//             var addPaymentMethodDto = new AddPaymentMethodDTO
-//             {
-//                 CardNumber = "4242424242424242",
-//                 ExpiryMonth = 9,
-//                 ExpiryYear = 2025,
-//                 CVC = "552",
-//             };
-//
-//             // Act
-//             _controller.AddPaymentMethod(user.Id, addPaymentMethodDto);
-//             var cards = _paymentService.GetCardsForUser(user);
-//
-//             // Assert
-//             Assert.True(cards.Any());
-//         }
-//
-//         [Fact(DisplayName = "AddPaymentMethod should return a NotFound when given a userId that is not in the database")]
-//         public async Task Test_AddPaymentMethod_ReturnsNotFoundWhenInvalidUserId()
-//         {
-//             // Arrange
-//             await _userCustomerService.AddStripeCustomerIdAsync(user);
-//
-//             var addPaymentMethodDto = new AddPaymentMethodDTO
-//             {
-//                 CardNumber = "4242424242424242",
-//                 ExpiryMonth = 9,
-//                 ExpiryYear = 2025,
-//                 CVC = "552",
-//             };
-//
-//             // Act
-//             var result = _controller.AddPaymentMethod(string.Empty, addPaymentMethodDto);
-//
-//             // Assert
-//             Assert.True(result.GetType() == typeof(NotFoundObjectResult));
-//         }
-//
-//         [Fact(DisplayName = "AddPaymentMethod should return a BadRequest when passed invalid card details")]
-//         public async Task Test_AddPaymentMethod_ReturnsBadRequestWhenInvalidCardDetails()
-//         {
-//             // Arrange
-//             await _userCustomerService.AddStripeCustomerIdAsync(user);
-//
-//             var addPaymentMethodDto = new AddPaymentMethodDTO
-//             {
-//                 CardNumber = "4242424242424242",
-//                 ExpiryMonth = 9,
-//                 ExpiryYear = 9999,
-//                 CVC = "552",
-//             };
-//
-//             // Act
-//             var result = _controller.AddPaymentMethod(user.Id, addPaymentMethodDto);
-//
-//             // Assert
-//             Assert.True(result.GetType() == typeof(BadRequestObjectResult));
-//         }
-//     }
-// }
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NectarineAPI.Controllers;
+using NectarineAPI.DTOs.Requests;
+using NectarineAPI.Services;
+using NectarineData.DataAccess;
+using NectarineData.Models;
+using Stripe;
+using Xunit;
+
+namespace NectarineTests.Controllers
+{
+    public class PaymentControllerTests
+    {
+        private readonly PaymentController _subject;
+        private readonly Mock<IPaymentService> _paymentServiceMock;
+        private readonly ApplicationUser user = new () { Id = Guid.NewGuid().ToString() };
+
+        public PaymentControllerTests()
+        {
+            // IPaymentServiceMock setup
+            _paymentServiceMock = new Mock<IPaymentService>();
+
+            _paymentServiceMock
+                .Setup(x => x.AddCardPaymentMethod(
+                    It.IsAny<ApplicationUser>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string>()));
+
+            // DbContext setup
+            var options = new DbContextOptionsBuilder<NectarineDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+
+            NectarineDbContext mockContext = new (options);
+            mockContext.Users.Add(user);
+            mockContext.SaveChanges();
+
+            // PaymentController setup
+            _subject = new PaymentController(mockContext, _paymentServiceMock.Object);
+        }
+
+        [Fact(DisplayName = "AddPaymentMethod should add a payment method to the user")]
+        public void Test_AddPaymentMethod()
+        {
+            // Arrange
+            var addPaymentMethodDto = new AddPaymentMethodDTO
+            {
+                CardNumber = "4242424242424242",
+                ExpiryMonth = 9,
+                ExpiryYear = 2025,
+                CVC = "552",
+            };
+
+            // Act
+            var result = _subject.AddPaymentMethod(user.Id, addPaymentMethodDto);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact(DisplayName = "AddPaymentMethod should return a NotFound when given a userId that is not in the database")]
+        public void Test_AddPaymentMethod_ReturnsNotFoundWhenInvalidUserId()
+        {
+            // Arrange
+            var addPaymentMethodDto = new AddPaymentMethodDTO
+            {
+                CardNumber = "4242424242424242",
+                ExpiryMonth = 9,
+                ExpiryYear = 2025,
+                CVC = "552",
+            };
+
+            // Act
+            var result = _subject.AddPaymentMethod(string.Empty, addPaymentMethodDto);
+
+            // Assert
+            Assert.True(result.GetType() == typeof(NotFoundObjectResult));
+        }
+
+        [Fact(DisplayName = "AddPaymentMethod should return a BadRequest when passed invalid card details")]
+        public void Test_AddPaymentMethod_ReturnsBadRequestWhenInvalidCardDetails()
+        {
+            // Arrange
+            _paymentServiceMock
+                .Setup(x => x.AddCardPaymentMethod(
+                    It.IsAny<ApplicationUser>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string>()))
+                .Returns(new StripeException());
+
+            var addPaymentMethodDto = new AddPaymentMethodDTO
+            {
+                CardNumber = "4242424242424242",
+                ExpiryMonth = 9,
+                ExpiryYear = 9999,
+                CVC = "552",
+            };
+
+            // Act
+            var result = _subject.AddPaymentMethod(user.Id, addPaymentMethodDto);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}
